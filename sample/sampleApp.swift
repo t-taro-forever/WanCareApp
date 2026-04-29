e83b9b34c9fe85b8e8166afdcc882a848236ff59
hdshirai@bbs.co.jp <hdshirai@bbs.co.jp>
2026-04-29 12:29:05 +0900

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by shirai on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
